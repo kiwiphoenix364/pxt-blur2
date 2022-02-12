4a82a7b99bd3a72a3814c52af6d21d8437f94a89
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-11 21:35:38 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -136,7 +136,7 @@ namespace Blur {
                     image.getRows(x2, buf)
                     for (let y2 = 0; y2 < 120; y2++) {
                         buf[y] = image.getPixel(x2, y2)
-                        image1.setRows(x2, buf)
+                        image.setRows(x2, buf)}}
                         numwidth = size
                         numheight = size
                         for (let index = 0; index < 120 / numheight; index++) {
@@ -146,14 +146,14 @@ namespace Blur {
                                 if (160 - x < size) {
                                     numwidth = 160 - x
                                 }
-                                image.fillRect(x, y, numwidth, numheight, image1.getPixel(x + numwidth / 2, y + numheight / 2))
+                                image.fillRect(x, y, numwidth, numheight, image.getPixel(x + numwidth / 2, y + numheight / 2))
                                 x += size
                                 numwidth = size
                                 x = 0
                                 y += size
                                 numheight = size
                             }
-                                }}}}
+                                }}
                                 }
                         })
     }
